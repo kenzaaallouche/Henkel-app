1a4e4195cbdbeb48097764208cc28694a0feb44e
kenzaaallouche <132127459+kenzaaallouche@users.noreply.github.com>
2024-04-04 17:55:32 +0100

    Update Henkel.py

--- Henkel.py
+++ Henkel.py
@@ -6,11 +6,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 
 # Chemin vers le Microsoft WebDriver. Assurez-vous de remplacer le chemin par celui où vous avez téléchargé le pilote.
-driver_path = r'C:/Users/deyha/Desktop/App Scrapping/msedgedriver.exe'
+driver_path = './msedgedriver.exe'
 
 # Options du navigateur Edge
 #edge_options = webdriver.EdgeOptions()
 
 # Initialisation du navigateur Edge avec les options
-driver = webdriver.Edge(executable_path=r'C:/Users/deyha/Desktop/App Scrapping/msedgedriver.exe')
+driver = webdriver.Edge(executable_path='./msedgedriver.exe')
 driver.get("http://158.69.241.112/SalesBuzzBO/ReportManager/Default.aspx")
