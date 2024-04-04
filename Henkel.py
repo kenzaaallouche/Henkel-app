08707e11e622232c5129ad8b9dbb1764b6e83bb2
kenzaaallouche <132127459+kenzaaallouche@users.noreply.github.com>
2024-04-04 18:15:43 +0100

    Update Henkel.py

--- Henkel.py
+++ Henkel.py
@@ -1,16 +1,35 @@
+import os
+import requests
+from zipfile import ZipFile
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import Select
 
-# Chemin vers le Microsoft WebDriver. Assurez-vous de remplacer le chemin par celui où vous avez téléchargé le pilote.
+# URL du pilote Microsoft Edge sur GitHub
+driver_url = 'https://github.com/kenzaaallouche/Henkel-app/blob/158855ef4d6a0f252ac4fc14cee5ee7db66c5f47/edgedriver_win64%20(3).zip'
 
+# Répertoire de téléchargement
+download_dir = './drivers'
 
-# Options du navigateur Edge
-#edge_options = webdriver.EdgeOptions()
+# Télécharger le pilote Edge depuis GitHub
+response = requests.get(driver_url)
+zip_file_path = os.path.join(download_dir, 'msedgedriver.zip')
+with open(zip_file_path, 'wb') as f:
+    f.write(response.content)
 
-# Initialisation du navigateur Edge avec les options
-driver = webdriver.Edge(executable_path='./msedgedriver.exe')
+# Extraire le pilote de l'archive ZIP
+with ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall(download_dir)
+
+# Ajouter le répertoire du pilote à la variable d'environnement PATH
+driver_path = os.path.join(download_dir, 'msedgedriver.exe')
+os.environ['PATH'] += os.pathsep + os.path.dirname(driver_path)
+
+# Démarrer le navigateur Edge
+driver = webdriver.Edge(executable_path=driver_path)
+
+# Exemple d'utilisation : naviguer vers une URL
 driver.get("http://158.69.241.112/SalesBuzzBO/ReportManager/Default.aspx")
+
+# Fermer le navigateur
+driver.quit()
+
+
