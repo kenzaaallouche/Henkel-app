1eff71202304a4a1d21f1f2ffce723c53a348eea
kenzaaallouche <132127459+kenzaaallouche@users.noreply.github.com>
2024-04-04 18:26:24 +0100

    Update Henkel.py

--- Henkel.py
+++ Henkel.py
@@ -3,24 +3,10 @@ import requests
 from zipfile import ZipFile
 from selenium import webdriver
 
-# URL du pilote Microsoft Edge sur GitHub
-driver_url = 'https://github.com/kenzaaallouche/Henkel-app/blob/158855ef4d6a0f252ac4fc14cee5ee7db66c5f47/edgedriver_win64%20(3).zip'
 
-# Répertoire de téléchargement
-download_dir = './drivers'
-
-# Télécharger le pilote Edge depuis GitHub
-response = requests.get(driver_url)
-zip_file_path = os.path.join('msedgedriver.zip')
-with open(zip_file_path, 'wb') as f:
-    f.write(response.content)
-
-# Extraire le pilote de l'archive ZIP
-with ZipFile(zip_file_path, 'r') as zip_ref:
-    zip_ref.extractall(download_dir)
 
 # Ajouter le répertoire du pilote à la variable d'environnement PATH
-driver_path = os.path.join(download_dir, 'msedgedriver.exe')
+driver_path = os.path.join('msedgedriver.exe')
 os.environ['PATH'] += os.pathsep + os.path.dirname(driver_path)
 
 # Démarrer le navigateur Edge
