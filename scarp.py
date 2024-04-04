c38eaff074713afa1917198f40c9243a1edb21bf
kenzaaallouche <132127459+kenzaaallouche@users.noreply.github.com>
2024-04-04 06:33:43 +0100

    Update scarp.py

--- scarp.py
+++ scarp.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from msedge.selenium_tools import Edge, EdgeOptions
 
 
 class TestKetest():
