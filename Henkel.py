0f51d5d41ad9db0db16a323541d96fe2616d9e9e
kenzaaallouche <132127459+kenzaaallouche@users.noreply.github.com>
2024-06-18 20:36:20 +0100

    Update Henkel.py

--- Henkel.py
+++ Henkel.py
@@ -2,20 +2,5 @@ import os
 import requests
 from zipfile import ZipFile
 from selenium import webdriver
-
-
-
-# Ajouter le répertoire du pilote à la variable d'environnement PATH
-driver_path = os.path.join('msedgedriver.exe')
-os.environ['PATH'] += os.pathsep + os.path.dirname(driver_path)
-
-# Démarrer le navigateur Edge
-driver = webdriver.Edge(executable_path=driver_path)
-
-# Exemple d'utilisation : naviguer vers une URL
-driver.get("http://158.69.241.112/SalesBuzzBO/ReportManager/Default.aspx")
-
-# Fermer le navigateur
-driver.quit()
-
-
+import streamlit as st
+st.title('SARL ANDROMED DISTRIBUTION -ALGER CENTRE- 2023')
